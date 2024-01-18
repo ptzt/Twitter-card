7540c4022dfd6cb782e461726fe4185c65cb3c75
Tomas <ignaciotomasrodriguezmillan@gmail.com>
2024-01-18 13:18:41 -0300

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import Card from "./components/Card"
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-      <h1>Twitter Card</h1>
-      <Card />
-    </div>
+    <article className='tw-followCard'>
+      <header className='tw-followCard-header'>
+        <img alt="El avatar del usuario" src="https://unavatar.io/ptztdev" className='tw-followCard-avatar' />
+        <div className='tw-followCard-info'>
+          <strong >Tomas Millan</strong>
+          <span className='tw-followCard-infoUserName'>@ptztdev</span>
+        </div>
+      </header>
+
+      <aside>
+        <button className='tw-followCard-button'>
+          Seguir
+        </button>
+      </aside>
+    </article>
   )
 }
 
